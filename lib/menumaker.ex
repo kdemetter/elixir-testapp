8b80ab3ea95eec8d0c57fb3cfb906af4e3b532b4
Kenny De Metter <kenny.demetter@colruytgroup.com>
2023-12-12 19:36:20 +0000

    fix: improvements

--- lib/menumaker.ex
+++ lib/menumaker.ex
@@ -1,13 +1,21 @@
 defmodule Menumaker do
 
+  @default_weekdays [:monday, :tuesday, :wednesday, :thursday, :friday, :saturday, :sunday]
+
+  def get_day_by_nr(day_of_week, weekdays \\ @default_weekdays)
+
+  def get_day_by_nr(day_of_week, weekdays) when day_of_week >= 0 and day_of_week <= length(weekdays) do
+      Enum.at(weekdays, day_of_week)
+  end
+
   def generate_week_menu(menus, nr_per_week \\ 7)
 
   def generate_week_menu(menus, nr_per_week) when length(menus) >= nr_per_week do
     Enum.take_random(menus,nr_per_week) |> Enum.with_index
   end
 
-  def generate_week_menu(menus, nr_per_week) do
-    raise "Menu must have at least #{nr_per_week} items"
+  def generate_week_menu(_menus, nr_per_week) do
+    raise ArgumentError, message: "menus must have at least #{nr_per_week} items"
   end
 
   def main do
@@ -16,7 +24,8 @@ defmodule Menumaker do
     Enum.each(1..10, fn(_x) ->
       week_menu = generate_week_menu(menus)
       Enum.each(week_menu, fn({entry, index}) ->
-        IO.puts("#{index}.#{entry}")
+        name_index = get_day_by_nr(index)
+        IO.puts("#{name_index}.#{entry}")
       end)
       IO.puts("---")
 
@@ -26,6 +35,3 @@ defmodule Menumaker do
   end
 
 end
-
-
-Menumaker.main()
--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Testapp.MixProject do
 
   def project do
     [
-      app: :testapp,
+      app: :menumaker,
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
